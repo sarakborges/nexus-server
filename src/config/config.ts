2aef0c4d2db0981a4381254473604b39f3d8a9ff
Sara Borges <sarakborges@outlook.com>
2025-06-04 11:40:00 -0300

    added more logs

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,10 @@ interface Config {
   atlasUri: string;
 }
 
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
+console.log(process.env.ATLAS_URI);
+
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || 'development',
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import config from './config/config.ts'; // Your config file with port, etc.
 import app from './app.ts'; // Your express app with routes defined
 
-const PORT = config.port || 3000;
+const PORT = config.port;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

b4efc748566c52c56e3f087e353a6aa382703e5f
Sara Borges <sarakborges@outlook.com>
2025-06-04 23:42:27 -0300

    added more routes awawa

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -24,13 +24,68 @@ export const createUser = async (
   }
 };
 
+// Read all profiles from user
+export const getProfileById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('Access GET /user/:id/profiles');
+
+  try {
+    const id = parseInt(req.params.id, 10);
+    const db = await getDb();
+    const collection = await db?.collection('profiles');
+    const profiles = await collection?.find({ userId: id });
+
+    if (!profiles) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    res.status(200).json(profiles);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Change which profile is active on user
+export const changeUserActiveProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('Access PATCH /users/:id/activeProfile');
+
+  try {
+    const { profile } = req.body;
+    const id = parseInt(req.params.id, 10);
+
+    const db = await getDb();
+    const collection = await db?.collection('users');
+    const user = await collection?.updateOne(
+      { id },
+      { $set: { activeProfile: profile } },
+    );
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    res.status(200).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
 // Add a new profile to an user
 export const addProfileToUser = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  console.log('Access PATCH /users/:id');
+  console.log('Access PATCH /users/:id/add');
 
   try {
     const { profile } = req.body;
@@ -41,6 +96,36 @@ export const addProfileToUser = async (
     const user = await collection?.updateOne(
       { id },
       { $push: { profiles: profile } },
+    );
+
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    res.status(200).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Remove an existing profile to an user
+export const removeProfileFromUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log('Access PATCH /users/:id/remove');
+
+  try {
+    const { profile } = req.body;
+    const id = parseInt(req.params.id, 10);
+
+    const db = await getDb();
+    const collection = await db?.collection('users');
+    const user = await collection?.updateOne(
+      { id },
+      { $pull: { profiles: profile } },
     );
 
     if (!user) {
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,6 +4,8 @@ import {
   doLogin,
   getMe,
   addProfileToUser,
+  removeProfileFromUser,
+  changeUserActiveProfile,
 } from '../controllers/userController.ts';
 
 const router = Router();
@@ -11,6 +13,8 @@ const router = Router();
 router.post('/login', doLogin);
 router.get('/:id', getMe);
 router.patch('/:id/add', addProfileToUser);
+router.patch('/:id/remove', removeProfileFromUser);
+router.patch('/:id/activeProfile', changeUserActiveProfile);
 router.post('/', createUser);
 
 export default router;

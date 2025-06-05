a0e5ebfbea1c67d221f8c4fbb3541080ec90baa9
Sara Borges <sarakborges@outlook.com>
2025-06-05 00:21:05 -0300

    made delete return more obvious

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -107,7 +107,7 @@ export const deleteProfileById = async (
     const collection = await db?.collection('profiles');
     const profile = await collection?.deleteOne({ id });
 
-    if (!profile) {
+    if (!profile.acknowledged) {
       res.status(404).json({ message: 'Profile not found' });
       return;
     }

9c8b896760154526ca58af144c87bf63742bd515
Sara Borges <sarakborges@outlook.com>
2025-06-09 02:30:36 -0300

    had duplicated instead of changing lmao

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,8 @@ import { errorHandler } from './middlewares/errorHandler.ts';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-
 app.use(express.json({ limit: '5mb' }));
+app.use(cors());
 
 // Routes
 app.use('/profiles', profileRoutes);

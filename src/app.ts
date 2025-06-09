3987b48fbce80a06c4343c471eda0560aff39952
Sara Borges <sarakborges@outlook.com>
2025-06-09 02:27:24 -0300

    adjusted json limit

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json({ limit: '5mb' }));
+
 // Routes
 app.use('/profiles', profileRoutes);
 app.use('/users', userRoutes);

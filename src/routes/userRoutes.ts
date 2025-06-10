d3e4b5272ffae5606127d56b69b28ff9713a22a8
Sara Borges <sarakborges@outlook.com>
2025-06-10 18:28:15 -0300

    removed unused route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,14 +6,12 @@ import {
   addProfileToUser,
   removeProfileFromUser,
   changeUserActiveProfile,
-  getProfilesByUser,
 } from '../controllers/userController.ts';
 
 const router = Router();
 
 router.post('/login', doLogin);
 router.get('/:id', getMe);
-router.get('/:id/profiles', getProfilesByUser);
 router.patch('/:id/add', addProfileToUser);
 router.patch('/:id/remove', removeProfileFromUser);
 router.patch('/:id/activeProfile', changeUserActiveProfile);

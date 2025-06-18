3f23ae0a2758ed699f26aab9b73df9fee6dc152a
Sara Borges <sarakborges@outlook.com>
2025-06-18 16:11:36 -0300

    made getprofilebyuri also fetch groups and connections

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -79,14 +79,146 @@ export const getProfileByUri = async (
     const uri = req.params.uri;
     const db = await getDb();
     const collection = await db?.collection('profiles');
-    const profile = await collection?.findOne({ uri });
+
+    function matchProfileByUri(uri: string) {
+      return { $match: { uri } };
+    }
+
+    function matchConnectedStatus() {
+      return {
+        $match: {
+          $expr: {
+            $and: [
+              { $eq: ['$status', 'connected'] },
+              { $in: ['$$profileId', '$between'] },
+            ],
+          },
+        },
+      };
+    }
+
+    function addOtherProfileId() {
+      return {
+        $addFields: {
+          otherProfileId: {
+            $cond: {
+              if: { $eq: ['$$profileId', { $arrayElemAt: ['$between', 0] }] },
+              then: { $arrayElemAt: ['$between', 1] },
+              else: { $arrayElemAt: ['$between', 0] },
+            },
+          },
+        },
+      };
+    }
+
+    function lookupOtherProfile() {
+      return {
+        $lookup: {
+          from: 'profiles',
+          localField: 'otherProfileId',
+          foreignField: '_id',
+          as: 'otherProfile',
+        },
+      };
+    }
+
+    function unwindOtherProfile() {
+      return { $unwind: '$otherProfile' };
+    }
+
+    function addOtherProfileField() {
+      return {
+        $addFields: {
+          otherProfile: '$otherProfile',
+        },
+      };
+    }
+
+    function lookupConnectedProfiles() {
+      return {
+        $lookup: {
+          from: 'connections',
+          let: { profileId: '$_id' },
+          pipeline: [
+            matchConnectedStatus(),
+            addOtherProfileId(),
+            lookupOtherProfile(),
+            unwindOtherProfile(),
+            addOtherProfileField(),
+          ],
+          as: 'connections',
+        },
+      };
+    }
+
+    function normalizeMembersArray() {
+      return {
+        $addFields: {
+          membersArray: {
+            $cond: [
+              { $isArray: '$members' },
+              '$members',
+              {
+                $cond: [
+                  { $gt: [{ $type: '$members' }, 'missing'] },
+                  [{ $ifNull: ['$members', {}] }],
+                  [],
+                ],
+              },
+            ],
+          },
+        },
+      };
+    }
+
+    function matchProfileInMembers() {
+      return {
+        $match: {
+          $expr: {
+            $gt: [
+              {
+                $size: {
+                  $filter: {
+                    input: '$membersArray',
+                    as: 'member',
+                    cond: {
+                      $eq: ['$$member.profileId', '$$profileId'],
+                    },
+                  },
+                },
+              },
+              0,
+            ],
+          },
+        },
+      };
+    }
+
+    function lookupGroupsByMembership() {
+      return {
+        $lookup: {
+          from: 'groups',
+          let: { profileId: '$_id' },
+          pipeline: [normalizeMembersArray(), matchProfileInMembers()],
+          as: 'groups',
+        },
+      };
+    }
+
+    const pipeline = [
+      matchProfileByUri(uri),
+      lookupConnectedProfiles(),
+      lookupGroupsByMembership(),
+    ];
+
+    const profile = await collection.aggregate(pipeline).toArray();
 
     if (!profile) {
       res.status(404).json({ message: 'Profile not found' });
       return;
     }
 
-    res.status(200).json(profile);
+    res.status(200).json(profile[0]);
   } catch (error) {
     next(error);
   }
